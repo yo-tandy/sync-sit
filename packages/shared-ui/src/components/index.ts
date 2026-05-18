289e024c92a655252adf0f1639d35b57f5072d8f
yo-tandy <yoav@taveyo.com>
2026-05-18 12:13:43 +0200

    feat(shared-ui): extract Checkbox

--- packages/shared-ui/src/components/index.ts
+++ packages/shared-ui/src/components/index.ts
@@ -2,3 +2,4 @@ export { Spinner } from './Spinner.js';
 export { Badge } from './Badge.js';
 export { Chip } from './Chip.js';
 export { Avatar } from './Avatar.js';
+export { Checkbox } from './Checkbox.js';
